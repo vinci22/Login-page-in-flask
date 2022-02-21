73ca4dddc46d1a9a536069b06c91f466b5a76b1e
vinci22 <dariojose22@outlook.com>
2022-02-21 11:25:38 -0500

    requerimientes for app

--- app.py
+++ app.py
@@ -2,17 +2,10 @@ from logging import debug
 from typing import List
 from flask import Flask as fk, request 
 from flask import Flask, render_template
-from Config.config import edit_route
 import urllib 
-
 
 
 app = fk(__name__)
 @app.route('/')
 def Register()-> str :
    return render_template ('Loggin.html',Tittlepage='Loggin')
-
-@app.route('/users')
-def getuser():
-   user = edit_route("users")
-   return user
