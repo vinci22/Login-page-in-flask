24676d2745a4f6d32491f49a8e55dce950dccb46
vinci22 <dariojose22@outlook.com>
2022-02-26 19:58:38 -0500

    application edit

--- application.py
+++ application.py
@@ -1,11 +1,8 @@
-from logging import debug
-from typing import List
+
 from flask import Flask as fk, request 
 from flask import Flask, render_template
-import urllib 
 
 
-<<<<<<< HEAD:application.py
 application = fk(__name__)
 @application.route('/')
 def Register()-> str :
@@ -14,9 +11,3 @@ def Register()-> str :
 if __name__ == '__main__':
    
    application.run(debug=True)
-=======
-app = fk(__name__)
-@app.route('/')
-def Register()-> str :
-   return render_template ('Loggin.html',Tittlepage='Loggin')
->>>>>>> dev:app.py
