b5b8745d6fff04ced4b1dedf85c008ddf3ed017e
vinci22 <dariojose22@outlook.com>
2022-02-26 19:06:59 -0500

    add new

--- application.py
+++ application.py
@@ -4,11 +4,11 @@ from flask import Flask as fk
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
-app = fk(__name__)
-@app.route('/')
+application = fk(__name__)
+@application.route('/')
 def Register()-> str :
    return render_template ('Loggin.html',Tittlepage='Loggin')
 
 if __name__ == '__main__':
    
-   app.run(debug=True)
+   application.run(debug=True)
